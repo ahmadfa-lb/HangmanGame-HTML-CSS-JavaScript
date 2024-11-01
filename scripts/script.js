310143e9a1169ec3d4b95ebcb81c8ffb834c86fa
ahmad farachi <103252037+ahmadfa-lb@users.noreply.github.com>
2024-11-01 12:47:08 +0200

    displaying a random word and hint/editing the style

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,17 @@
+const wordDisplay = document.querySelector(".word-display")
 const keyboardDiv = document.querySelector(".keyboard")
 
+const getRandomWord = () =>{
+    const { word, hint } = wordList[Math.floor(Math.random() * wordList.length)];
+    document.querySelector(".hint-text b").innerText = hint;
+    wordDisplay.innerHTML = word.split("").map(() => `<li class="letter"></li>`).join("");
+}
 
-//creating keyboard buttons
 for(let i = 97; i <= 122; i++){
     const button = document.createElement("button");
     button.innerText = String.fromCharCode(i);
     keyboardDiv.appendChild(button);
 }
+
+
+getRandomWord();
